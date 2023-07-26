e061bb8653afaa83ef48e34f05c2bf9224db6913
duckwithnosound <vantasi2000@gmail.com>
2023-07-26 22:40:06 +0700

    Change consol output to log

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.logging.Logger;
+
 public class Main {
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        logger.info("Design Pattern");
     }
 }
