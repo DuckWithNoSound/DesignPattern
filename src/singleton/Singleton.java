ee702825a1eb7f84f7cd4381161dbc3fe4547400
duckwithnosound <vantasi2000@gmail.com>
2023-07-27 22:45:40 +0700

    [FEAT] Add Singleton Design Pattern

---
+++ src/singleton/Singleton.java
@@ -0,0 +1,27 @@
+package singleton;
+
+public class Singleton {
+
+    private static Singleton singleton;
+
+    private Singleton(){}
+
+    /**
+     * Singleton Design Pattern
+     * Only one instance of class can be existed on all the time program running.
+     * The instance of class is global access.
+     * The instance of class is initialized only when it's requested for the first time.
+     * @return an instance of Singleton class
+     */
+    public static Singleton getInstance() {
+        if(singleton == null) {
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+
+    public String doStaff () {
+        return ": Do staff.";
+    }
+
+}
---
+++ src/singleton/SingletonTest.java
@@ -0,0 +1,29 @@
+package singleton;
+
+import java.util.logging.Logger;
+
+public class SingletonTest {
+
+    private static final Logger logger = Logger.getLogger(SingletonTest.class.getName());
+
+    private SingletonTest(){}
+
+    /**
+     * This function used to test Singleton Design Pattern.
+     */
+    public static void run() {
+
+        // Create instance 1
+        Singleton singleton1 = Singleton.getInstance();
+        // Do staff by instance 1
+        logger.info(singleton1 + singleton1.doStaff());
+
+        // Create instance 2
+        Singleton singleton2 = Singleton.getInstance();
+        // Do staff by instance 2
+        logger.info(singleton2 + singleton2.doStaff());
+
+        // The output is only one instance do staff 2 times
+    }
+
+}
